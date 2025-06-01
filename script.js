e2773685563a6e7bc0c398818a367eec916de09d
AbdulQadir Memon <125130533+Qadirmemon90@users.noreply.github.com>
2025-06-01 07:45:30 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -5,3 +5,30 @@ menu.onclick = () => {
     menu.classList.toggle('bx-x');
     nav.classList.toggle('active');
 }
+
+var url = 'https://cdn.waplus.io/waplus-crm/settings/ossembed.js';
+var s = document.createElement('script');
+s.type = 'text/javascript';
+s.async = true;
+s.src = url;
+var options = {
+"enabled": true,
+"chatButtonSetting": {
+"backgroundColor": "#16BE45",
+"ctaText": "Message Us",
+"borderRadius": "8",
+"marginLeft": "20",
+"marginBottom": "20",
+"marginRight": "20",
+"position": "right",
+"textColor": "#ffffff",
+"phoneNumber": "+923392006182",
+"messageText": "👋🏻Hello",
+"trackClick": true
+}
+}
+s.onload = function() {
+CreateWhatsappBtn(options);
+};
+var x = document.getElementsByTagName('script')[0];
+x.parentNode.insertBefore(s, x);
